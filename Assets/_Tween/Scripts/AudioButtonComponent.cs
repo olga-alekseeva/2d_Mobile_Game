a3c8bfb1b60a418aef5f62f194a160c31b697bfb
olga-alekseeva <oliasca@gmail.com>
2022-06-26 13:38:44 +0300

    +

---
+++ Assets/_Tween/Scripts/AudioButtonComponent.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Tween
+{
+    [RequireComponent(typeof(Button))]
+    [RequireComponent(typeof(AudioSource))]
+    [RequireComponent(typeof(RectTransform))]
+    public class AudioButtonComponent : MonoBehaviour
+    {
+        [Header("Components")]
+        [SerializeField] private Button _button;
+        [SerializeField] private AudioSource _audioSource;
+        [SerializeField] private RectTransform _rectTransform;
+
+        private void OnValidate() => InitComponents();
+        private void Awake() => InitComponents();
+
+        private void Start() => _button.onClick.AddListener(OnButtonClick);
+        private void OnDestroy() => _button.onClick.RemoveAllListeners();
+
+        private void InitComponents()
+        {
+            _button ??= GetComponent<Button>();
+            _audioSource ??= GetComponent<AudioSource>();
+            _rectTransform ??= GetComponent<RectTransform>();
+        }
+        private void OnButtonClick()
+        {
+            PlaySound();
+        }
+        private void PlaySound() => _audioSource.Play();
+    }
+}
